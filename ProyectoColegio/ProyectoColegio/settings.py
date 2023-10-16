acb9964b6ddb6352128472d5d7d80440a98e74cd
Empowerdeith <clone10700@hotmail.com>
2023-10-16 00:40:13 -0300

    mejoras backend, mejoras utiles, integración de reglamento

--- ProyectoColegio/ProyectoColegio/settings.py
+++ ProyectoColegio/ProyectoColegio/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "mainApp"
+    "mainApp",
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
