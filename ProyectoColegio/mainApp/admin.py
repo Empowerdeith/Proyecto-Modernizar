9b6a2f918317af5bba903afd3559077791664a7e
Administrador <builder2890@outlook.com>
2023-11-09 15:03:24 -0300

    mejoras seccion botones utiles

--- ProyectoColegio/mainApp/admin.py
+++ ProyectoColegio/mainApp/admin.py
@@ -1,18 +1,25 @@
 from django.contrib import admin
 from .models import Reglamento, ToolsLeft, ToolsCenter, ToolsRight
-"""from .models import NivTransicion
-from .models import EdBasica"""
+
 
 admin.site.register(Reglamento)
-admin.site.register(ToolsLeft)
-admin.site.register(ToolsCenter)
-admin.site.register(ToolsRight)
-
-"""@admin.register(EdBasica)
-class EdBasicaAdmin(admin.ModelAdmin):
-    list_display = ('edbasica','utilesCurso')
-    ordering = ('edbasica',)
-    search_fields =('edbasica',)"""
 
 
-#admin.site.register(EdBasica,EdBasicaAdmin)
+@admin.register(ToolsLeft)
+class ToolsLeftAdmin(admin.ModelAdmin):
+    list_display = ('nombre','utilesCurso')
+    ordering = ('nombre',)
+    search_fields =('nombre',)
+
+@admin.register(ToolsRight)
+class ToolsRightAdmin(admin.ModelAdmin):
+    list_display = ('nombre','utilesCurso')
+    ordering = ('nombre',)
+    search_fields =('nombre',)
+    
+    
+@admin.register(ToolsCenter)
+class ToolsCenterAdmin(admin.ModelAdmin):
+    list_display = ('nombre','utilesCurso')
+    ordering = ('nombre',)
+    search_fields =('nombre',)
