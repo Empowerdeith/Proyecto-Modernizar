9d2609db24e399eb02ec0f540812897453609fb4
Empowerdeith <clone10700@hotmail.com>
2023-11-19 19:32:19 -0300

    changes to visual of landing page

--- ProyectoColegio/ProyectoColegio/settings.py
+++ ProyectoColegio/ProyectoColegio/settings.py
@@ -128,5 +128,3 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
