3b64bb9f0a919d1db737de43fb24a8e643a563bf
Empowerdeith <clone10700@hotmail.com>
2023-11-30 19:36:11 -0300

    switch to production sv

--- ProyectoColegio/ProyectoColegio/settings.py
+++ ProyectoColegio/ProyectoColegio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$j%1cydine_)y6-q+m7t+a!ex_3er-=wmxd-%v+8bqf1eb^bbx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -124,6 +124,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
